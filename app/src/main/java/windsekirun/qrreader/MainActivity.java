14f68ff688759dba3a7e9a4cdd61bfdfa3156222
WindSekirun <windsekirun@live.co.kr>
2015-04-07 16:33:43 +0900

    incomplete put data to server.

--- app/src/main/java/windsekirun/qrreader/MainActivity.java
+++ app/src/main/java/windsekirun/qrreader/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
@@ -17,8 +18,28 @@ import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.Toast;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+
+import windsekirun.qrreader.async.NaraeAsync;
+import windsekirun.qrreader.db.TempResultAdapter;
+import windsekirun.qrreader.util.JsonReceiver;
+
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+    TempResultAdapter adapter;
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -37,10 +58,13 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         Button generate = (Button) findViewById(R.id.generate);
         Button capture = (Button) findViewById(R.id.read);
         Button license = (Button) findViewById(R.id.license);
+        Button send = (Button) findViewById(R.id.serverSend);
         setSupportActionBar(toolbar);
         generate.setOnClickListener(this);
         capture.setOnClickListener(this);
         license.setOnClickListener(this);
+        send.setOnClickListener(this);
+        adapter = new TempResultAdapter(this);
     }
 
     @Override
@@ -52,8 +76,10 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         } else if (id == R.id.read) {
             startActivity(new Intent(MainActivity.this, CaptureActivity.class));
             finish();
-        } else {
+        } else if (id == R.id.license) {
             showLicenseDialog();
+        } else {
+            showSendAlert();
         }
     }
 
@@ -70,6 +96,112 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
             }
         });
         ab.show();
+    }
+
+    public void showSendAlert() {
+        AlertDialog.Builder ab = new AlertDialog.Builder(this);
+        ab.setMessage("서버로 데이터를 정말 보내시겠습니까?");
+        ab.setPositiveButton("보내기", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                new NaraeAsync(false, new SendData(), 8, "SEND_DATA").execute();
+            }
+        });
+        ab.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+    }
+
+    public class SendData implements Runnable {
+        ArrayList<String> qrlist;
+        ArrayList<String> receivedList = new ArrayList<>();
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+        @Override
+        public void run() {
+            // get SQLite list
+            try {
+                adapter.open();
+                qrlist = adapter.getText();
+                adapter.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            // Receive Server Form data.
+            // TODO: implement Receive PHP
+            JsonReceiver sh = new JsonReceiver();
+            String jsonStr = sh.makeJsonCall("", JsonReceiver.GET);
+            if (jsonStr != null) {
+                JSONObject jsonObj = null;
+                try {
+                    jsonObj = new JSONObject(jsonStr);
+                    JSONArray data = jsonObj.getJSONArray("data");
+                    for (int i = 0; i < data.length(); i++) {
+                        JSONObject c = data.getJSONObject(i);
+                        String receivedData = c.getString("studentNum");
+                        receivedList.add(receivedData);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            // put datas into Hashmap
+            for (int i = 0; i < receivedList.size(); i++) map.put(receivedList.get(i), 0);
+
+            // contract map <-> qrlist
+            for (int i = 0; i < qrlist.size(); i++) map.put(qrlist.get(i), 1);
+
+            // Create Table follows Date
+            makeDateTable(getTime());
+
+            // put hashmap to server
+            // TODO: implement put hashmap to server
+
+            // SQLite list clear
+            try {
+                adapter.open();
+                adapter.clear();
+                adapter.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(MainActivity.this, "서버로 데이터를 보냈습니다!", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+
+    public HttpPost makeDateTable(String dateTableName) {
+        // TODO: implement Create Table PHP
+        HttpPost request = new HttpPost("");
+        ArrayList<NameValuePair> nameValue = new ArrayList<>();
+        nameValue.add(new BasicNameValuePair("name", dateTableName));
+        request.setEntity(makeEntity(nameValue));
+        return request;
+    }
+
+    public HttpEntity makeEntity(ArrayList<NameValuePair> $nameValue){
+        HttpEntity result = null;
+        try{
+            result = new UrlEncodedFormEntity($nameValue,"UTF-8");
+        } catch (UnsupportedEncodingException e){
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    protected String getTime() {
+        return DateFormat.format("yyyyMMdd", Calendar.getInstance().getTime()).toString();
     }
 
 }
---
+++ app/src/main/java/windsekirun/qrreader/util/JsonReceiver.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2014 LHCS, All Rights Reversed
+ *
+ * Maintainer:
+ *
+ * LHCS (Admin) lhcs0916@gmail.com
+ * WindSekirun (Developer) windsekirun@icloud.com (windsekirun@live.co.kr)
+ *
+ * License: NOT OPENED
+ */
+package windsekirun.qrreader.util;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.List;
+
+@SuppressWarnings({"SameParameterValue", "WeakerAccess"})
+public class JsonReceiver {
+
+    static String response = null;
+    public final static int GET = 1;
+    public final static int POST = 2;
+
+    public JsonReceiver() {
+
+    }
+
+    public String makeJsonCall(String url, int method) {
+        return this.makeJsonCall(url, method, null);
+    }
+
+    public String makeJsonCall(String url, int method, List<NameValuePair> params) {
+        try {
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpEntity httpEntity;
+            HttpResponse httpResponse = null;
+
+            if (method == POST) {
+                HttpPost httpPost = new HttpPost(url);
+                if (params != null) {
+                    httpPost.setEntity(new UrlEncodedFormEntity(params));
+                }
+
+                httpResponse = httpClient.execute(httpPost);
+
+            } else if (method == GET) {
+                if (params != null) {
+                    String paramString = URLEncodedUtils.format(params, "utf-8");
+                    url += "?" + paramString;
+                }
+                HttpGet httpGet = new HttpGet(url);
+
+                httpResponse = httpClient.execute(httpGet);
+
+            }
+            assert httpResponse != null;
+            httpEntity = httpResponse.getEntity();
+            response = EntityUtils.toString(httpEntity);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return response;
+
+    }
+}
